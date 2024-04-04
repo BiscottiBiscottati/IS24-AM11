adff055e59fc4bc237f3d73d46da4e7f96c526ea
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-04-04 16:35:17 +0200

    GoldDeck simple implementation and DatabaseSearchable for some enum

--- src/main/java/it/polimi/ingsw/am11/cards/utils/enums/Color.java
+++ src/main/java/it/polimi/ingsw/am11/cards/utils/enums/Color.java
@@ -2,20 +2,31 @@ package it.polimi.ingsw.am11.cards.utils.enums;
 
 import it.polimi.ingsw.am11.cards.utils.CornerContainer;
 import it.polimi.ingsw.am11.cards.utils.Item;
+import it.polimi.ingsw.am11.decks.DatabaseSearchable;
 
 /**
  * Can represent either the color of a card or
  * the colored symbols contained in a corner or center of a card.
  */
-public enum Color implements CornerContainer, Item {
-    RED,
-    BLUE,
-    GREEN,
-    PURPLE;
+public enum Color implements CornerContainer, Item, DatabaseSearchable {
+    RED("red"),
+    BLUE("blue"),
+    GREEN("green"),
+    PURPLE("purple");
 
+    private final String columnName;
+
+    Color(String columnName) {
+        this.columnName = columnName;
+    }
 
     @Override
     public boolean isAvailable() {
         return true;
+    }
+
+    @Override
+    public String getColumnName() {
+        return this.columnName;
     }
 }
---
+++ src/main/java/it/polimi/ingsw/am11/decks/DatabaseSearchable.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.am11.decks;
+
+public interface DatabaseSearchable {
+    String getColumnName();
+}
