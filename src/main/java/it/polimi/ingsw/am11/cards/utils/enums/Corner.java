45061d90fa721f8ab00f47843dae6688d342dea2
Ferdi <ferdinandomaria.cioffi@mail.polimi.it>
2024-04-04 18:12:08 +0200

    some change for rebase

--- src/main/java/it/polimi/ingsw/am11/cards/utils/enums/Corner.java
+++ src/main/java/it/polimi/ingsw/am11/cards/utils/enums/Corner.java
@@ -1,12 +1,24 @@
 package it.polimi.ingsw.am11.cards.utils.enums;
 
+import it.polimi.ingsw.am11.decks.DatabaseSearchable;
+
 /**
  * Represent the four corners of a card.
  */
-public enum Corner {
-    DOWN_LX,
-    DOWN_RX,
-    TOP_LX,
-    TOP_RX
+public enum Corner implements DatabaseSearchable {
+    DOWN_LX("down_lx"),
+    DOWN_RX("down_rx"),
+    TOP_LX("top_lx"),
+    TOP_RX("top_rx");
 
+    private final String columnName;
+
+    Corner(String columnName) {
+        this.columnName = columnName;
+    }
+
+    @Override
+    public String getColumnName() {
+        return this.columnName;
+    }
 }
--- src/main/java/it/polimi/ingsw/am11/decks/playable/GoldDeck.java
+++ src/main/java/it/polimi/ingsw/am11/decks/playable/GoldDeck.java
@@ -5,30 +5,40 @@ import it.polimi.ingsw.am11.decks.Deck;
 import it.polimi.ingsw.am11.decks.utils.DeckType;
 import it.polimi.ingsw.am11.decks.utils.PlayableDeckType;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
 
 public class GoldDeck implements Deck<GoldCard> {
 
-    private ArrayList<GoldCard> goldDeck;
+    private final Stack<GoldCard> goldDeck;
+
+    public GoldDeck() {
+        goldDeck = new Stack<>();
+    }
 
     @Override
     public Deck<GoldCard> shuffle() {
-        return null;
+        Collections.shuffle(goldDeck);
+        return this;
     }
 
     @Override
     public GoldCard drawCard() {
-        return null;
+        return goldDeck.pop();
+    }
+
+    public boolean isEmpty() {
+        return goldDeck.isEmpty();
     }
 
     @Override
     public int getRemainingCards() {
-        return 0;
+        return goldDeck.size();
     }
 
     @Override
     public void addCard(GoldCard card) {
-
+        goldDeck.push(card);
     }
 
     @Override
