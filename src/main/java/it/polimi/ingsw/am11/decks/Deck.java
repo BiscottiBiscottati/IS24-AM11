db8bfaafddc6ff76917386e88cba3d2c410274d4
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-04-07 17:12:27 +0200

    added javadoc for Deck

--- src/main/java/it/polimi/ingsw/am11/decks/Deck.java
+++ src/main/java/it/polimi/ingsw/am11/decks/Deck.java
@@ -8,6 +8,11 @@ import java.util.Collections;
 import java.util.Optional;
 import java.util.Stack;
 
+/**
+ * Represents a deck of cards.
+ *
+ * @param <T> The type of card that the deck contains.
+ */
 public class Deck<T extends CardIdentity> {
     private final ImmutableMap<Integer, T> mappingIdToCard;
 
@@ -20,24 +25,50 @@ public class Deck<T extends CardIdentity> {
     }
 
 
+    /**
+     * Shuffles the deck and returns it.
+     *
+     * @return The deck shuffled.
+     */
     public Deck<T> shuffle() {
         Collections.shuffle(this.deck);
         return this;
     }
 
+    /**
+     * Method to draw a card from the deck.
+     * <p>
+     * The card is removed from the deck.
+     *
+     * @return The card on top of the deck.
+     */
     public T drawCard() {
         return this.deck.pop();
     }
 
+    /**
+     * Method to get the number of cards remaining in the deck.
+     *
+     * @return The number of cards remaining in the deck.
+     */
     public int getRemainingCards() {
         return this.deck.size();
     }
 
+    /**
+     * Method to reset the deck based on its initial idMapping.
+     */
     public void reset() {
         this.deck.clear();
         this.deck.addAll(mappingIdToCard.values());
     }
 
+    /**
+     * Method to get a card by its id.
+     *
+     * @param id The id of the card to get.
+     * @return The card with the given id, if present.
+     */
     Optional<T> getCardById(int id) {
         return Optional.ofNullable(mappingIdToCard.getOrDefault(id, null));
     }
