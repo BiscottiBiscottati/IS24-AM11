90c1cf6bd7d61c322ffb30f43d8a3176faba042c
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-04-07 10:33:18 +0200

    added method to get column name for symbols

--- src/main/java/it/polimi/ingsw/am11/cards/utils/enums/Symbol.java
+++ src/main/java/it/polimi/ingsw/am11/cards/utils/enums/Symbol.java
@@ -7,10 +7,19 @@ import it.polimi.ingsw.am11.cards.utils.Item;
  * Represents the uncolored symbols contained in the corners of a card.
  */
 public enum Symbol implements CornerContainer, Item {
-    FEATHER,
-    GLASS,
-    PAPER;
+    FEATHER("feather"),
+    GLASS("glass"),
+    PAPER("paper");
 
+    private final String columnName;
+
+    Symbol(String columnName) {
+        this.columnName = columnName;
+    }
+
+    public String getColumnName() {
+        return this.columnName;
+    }
 
     @Override
     public boolean isAvailable() {
