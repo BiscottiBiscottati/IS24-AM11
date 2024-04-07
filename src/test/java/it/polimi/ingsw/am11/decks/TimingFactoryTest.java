9bf0e5ad9719db9627ce0bb8c224d7a65096370b
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-04-07 12:06:50 +0200

    added Timing testing for curiosity temporary

---
+++ src/test/java/it/polimi/ingsw/am11/decks/TimingFactoryTest.java
@@ -0,0 +1,59 @@
+package it.polimi.ingsw.am11.decks;
+
+import it.polimi.ingsw.am11.decks.objective.ObjectiveDeckFactory;
+import it.polimi.ingsw.am11.decks.playable.GoldDeckFactory;
+import it.polimi.ingsw.am11.decks.playable.ResourceDeckFactory;
+import it.polimi.ingsw.am11.decks.starter.StarterDeckFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class TimingFactoryTest {
+
+    NumberFormat formatter;
+
+    private static double getNumber(long end, long start) {
+        return (double) ((end - start) / 1000) / 1000000;
+    }
+
+    @BeforeEach
+    void setUp() {
+        formatter = NumberFormat.getNumberInstance(Locale.ITALY);
+        formatter.setMinimumFractionDigits(4);
+    }
+
+    @Test
+    void testTiming() {
+
+        long start = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            ResourceDeckFactory.createDeck();
+        }
+        long end = System.nanoTime();
+        System.out.println("average timing Resource = " + formatter.format(getNumber(end, start)) + " ms");
+
+        start = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            StarterDeckFactory.createDeck();
+        }
+        end = System.nanoTime();
+        System.out.println("average timing Starter = " + formatter.format(getNumber(end, start)) + " ms");
+
+        start = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            GoldDeckFactory.createDeck();
+        }
+        end = System.nanoTime();
+        System.out.println("average timing Gold = " + formatter.format(getNumber(end, start)) + " ms");
+
+        start = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            ObjectiveDeckFactory.createDeck();
+        }
+        end = System.nanoTime();
+        System.out.println("average timing Objective = " + formatter.format(getNumber(end, start)) + " ms");
+
+    }
+}
