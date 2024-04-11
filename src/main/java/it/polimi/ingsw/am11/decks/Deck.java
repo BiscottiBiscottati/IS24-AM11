e0302b4b5fad729a709501ec7e0b83977ea7059e
EdoardoBergamoMac <edoardo.bergamo@mail.polimi.it>
2024-04-11 19:48:52 +0200

    Added getTopCard method to Deck class

--- src/main/java/it/polimi/ingsw/am11/decks/Deck.java
+++ src/main/java/it/polimi/ingsw/am11/decks/Deck.java
@@ -90,4 +90,7 @@ public class Deck<T extends CardIdentity> {
         return Optional.ofNullable(mappingIdToCard.getOrDefault(id, null));
     }
 
+    public Optional<T> getTopCard() {
+        return Optional.ofNullable(deck.peek());
+    }
 }
