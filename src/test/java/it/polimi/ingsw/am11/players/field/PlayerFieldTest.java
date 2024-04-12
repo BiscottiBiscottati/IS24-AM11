e7b07b29a611259ecb9ee96a3c2fd6f99cf64e0e
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-04-12 21:09:39 +0200

    used static imports on PlayerFieldTest

--- src/test/java/it/polimi/ingsw/am11/players/field/PlayerFieldTest.java
+++ src/test/java/it/polimi/ingsw/am11/players/field/PlayerFieldTest.java
@@ -25,6 +25,8 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class PlayerFieldTest {
     static Deck<ResourceCard> resourceDeck;
     static Deck<StarterCard> starterDeck;
@@ -50,39 +52,39 @@ class PlayerFieldTest {
     @Test
     void getCardsPositioned() {
         Map<Position, CardContainer> positionedCards = playerField.getCardsPositioned();
-        Assertions.assertTrue(positionedCards.isEmpty());
+        assertTrue(positionedCards.isEmpty());
 
         StarterCard starter = starterDeck.draw().orElseThrow();
-        Assertions.assertDoesNotThrow(() -> playerField.placeStartingCard(starter, true));
+        assertDoesNotThrow(() -> playerField.placeStartingCard(starter, true));
         positionedCards = playerField.getCardsPositioned();
-        Assertions.assertEquals(1, positionedCards.size());
-        Assertions.assertTrue(positionedCards.containsKey(Position.of(0, 0)));
-        Assertions.assertTrue(playerField.containsCard(starter));
+        assertEquals(1, positionedCards.size());
+        assertTrue(positionedCards.containsKey(Position.of(0, 0)));
+        assertTrue(playerField.containsCard(starter));
 
         ResourceCard resource = resourceDeck.draw().orElseThrow();
-        Assertions.assertDoesNotThrow(() -> playerField.place(resource, Position.of(1, 1), false));
+        assertDoesNotThrow(() -> playerField.place(resource, Position.of(1, 1), false));
         positionedCards = playerField.getCardsPositioned();
-        Assertions.assertEquals(2, positionedCards.size());
-        Assertions.assertTrue(positionedCards.containsKey(Position.of(1, 1)));
-        Assertions.assertTrue(positionedCards.containsKey(Position.of(0, 0)));
-        Assertions.assertTrue(playerField.containsCard(resource));
-        Assertions.assertTrue(playerField.containsCard(starter));
+        assertEquals(2, positionedCards.size());
+        assertTrue(positionedCards.containsKey(Position.of(1, 1)));
+        assertTrue(positionedCards.containsKey(Position.of(0, 0)));
+        assertTrue(playerField.containsCard(resource));
+        assertTrue(playerField.containsCard(starter));
     }
 
     @Test
     void getPlacedCardColours() {
         playerField.getPlacedCardColours()
-                   .forEach((color, integer) -> Assertions.assertEquals(0, integer));
+                   .forEach((color, integer) -> assertEquals(0, integer));
 
         StarterCard starter = starterDeck.draw().orElseThrow();
-        Assertions.assertDoesNotThrow(() -> playerField.placeStartingCard(starter, true));
+        assertDoesNotThrow(() -> playerField.placeStartingCard(starter, true));
         playerField.getPlacedCardColours()
-                   .forEach((color, integer) -> Assertions.assertEquals(0, integer));
+                   .forEach((color, integer) -> assertEquals(0, integer));
 
         ResourceCard resourceCard = resourceDeck.draw().orElseThrow();
         Color colorOfCard = resourceCard.getColor();
-        Assertions.assertDoesNotThrow(() -> playerField.place(resourceCard, Position.of(1, 1), false));
-        Assertions.assertEquals(1, playerField.getPlacedCardColours().get(colorOfCard));
+        assertDoesNotThrow(() -> playerField.place(resourceCard, Position.of(1, 1), false));
+        assertEquals(1, playerField.getPlacedCardColours().get(colorOfCard));
     }
 
     @Test
@@ -90,16 +92,16 @@ class PlayerFieldTest {
         StarterCard starterCard = starterDeck.draw().orElseThrow();
 
         // Testing placing a StarterCard on its retro
-        Assertions.assertDoesNotThrow(() -> playerField.placeStartingCard(starterCard, true));
+        assertDoesNotThrow(() -> playerField.placeStartingCard(starterCard, true));
 
         // Checking that the card is placed and the position is not available
-        Assertions.assertFalse(playerField.isAvailable(Position.of(0, 0)));
-        Assertions.assertTrue(playerField.containsCard(starterCard));
+        assertFalse(playerField.isAvailable(Position.of(0, 0)));
+        assertTrue(playerField.containsCard(starterCard));
 
         // Testing illegal positioning of another starter
         StarterCard secondStarter = starterDeck.draw().orElseThrow();
-        Assertions.assertThrows(IllegalCardPlacingException.class,
-                                () -> playerField.placeStartingCard(secondStarter, false));
+        assertThrows(IllegalCardPlacingException.class,
+                     () -> playerField.placeStartingCard(secondStarter, false));
     }
 
     @Test
@@ -111,23 +113,23 @@ class PlayerFieldTest {
         int pointsExpected;
 
         // Testing placing a card on the starter position
-        Assertions.assertThrows(IllegalCardPlacingException.class,
-                                () -> pointsGiven.set(
-                                        playerField.place(resourceCard,
-                                                          Position.of(0, 0),
-                                                          false)));
+        assertThrows(IllegalCardPlacingException.class,
+                     () -> pointsGiven.set(
+                             playerField.place(resourceCard,
+                                               Position.of(0, 0),
+                                               false)));
 
         // Checking that no points have been given
-        Assertions.assertEquals(0, pointsGiven.get());
+        assertEquals(0, pointsGiven.get());
 
         // Placing a StarterCard on its retro
         StarterCard starter = starterDeck.draw().orElseThrow();
         placedCards.add(starter);
-        Assertions.assertDoesNotThrow(() -> playerField.placeStartingCard(starter, true));
+        assertDoesNotThrow(() -> playerField.placeStartingCard(starter, true));
         placedPos.add(Position.of(0, 0));
 
         // Placing a ResourceCard on (1, 1)
-        Assertions.assertDoesNotThrow(() -> pointsGiven.set(
+        assertDoesNotThrow(() -> pointsGiven.set(
                 playerField.place(resourceCard, Position.of(1, 1), false))
         );
         placedCards.add(resourceCard);
@@ -135,27 +137,27 @@ class PlayerFieldTest {
 
         // Checking that the card gives the expected points
         pointsExpected = resourceCard.getPoints();
-        Assertions.assertEquals(pointsExpected, pointsGiven.get());
+        assertEquals(pointsExpected, pointsGiven.get());
 
         // Checking that the cards are placed and the positions are not available
-        placedPos.forEach(pos -> Assertions.assertFalse(playerField.isAvailable(pos)));
-        placedCards.forEach(card -> Assertions.assertTrue(playerField.containsCard(card)));
+        placedPos.forEach(pos -> assertFalse(playerField.isAvailable(pos)));
+        placedCards.forEach(card -> assertTrue(playerField.containsCard(card)));
 
         // Testing illegal positioning to same position
-        Assertions.assertThrows(IllegalCardPlacingException.class,
-                                () -> playerField.place(resourceCard,
-                                                        Position.of(1, 1),
-                                                        false));
+        assertThrows(IllegalCardPlacingException.class,
+                     () -> playerField.place(resourceCard,
+                                             Position.of(1, 1),
+                                             false));
 
         // Testing illegal positioning to starter position
-        Assertions.assertThrows(IllegalCardPlacingException.class,
-                                () -> playerField.place(resourceCard,
-                                                        Position.of(0, 0),
-                                                        false));
+        assertThrows(IllegalCardPlacingException.class,
+                     () -> playerField.place(resourceCard,
+                                             Position.of(0, 0),
+                                             false));
 
         // Checking nothing has changed from the previous state
-        placedCards.forEach(card -> Assertions.assertTrue(playerField.containsCard(card)));
-        placedPos.forEach(pos -> Assertions.assertFalse(playerField.isAvailable(pos)));
+        placedCards.forEach(card -> assertTrue(playerField.containsCard(card)));
+        placedPos.forEach(pos -> assertFalse(playerField.isAvailable(pos)));
 
         // Testing random placements on its retro
         for (int i = 0; i < 4; i++) {
@@ -169,19 +171,19 @@ class PlayerFieldTest {
             for (Position position : posToPlace) {
                 ResourceCard card = resourceDeck.draw().orElseThrow();
                 placedCards.add(card);
-                Assertions.assertDoesNotThrow(() -> pointsGiven.set(
+                assertDoesNotThrow(() -> pointsGiven.set(
                         playerField.place(card, position, true))
                 );
                 placedPos.add(position);
 
                 // Checking that the card gives the expected points
                 pointsExpected = card.getPoints();
-                Assertions.assertEquals(pointsExpected, pointsGiven.get());
+                assertEquals(pointsExpected, pointsGiven.get());
             }
 
             // Checking that the cards are placed and the positions are not available
-            placedCards.forEach(card -> Assertions.assertTrue(playerField.containsCard(card)));
-            placedPos.forEach(pos -> Assertions.assertFalse(playerField.isAvailable(pos)));
+            placedCards.forEach(card -> assertTrue(playerField.containsCard(card)));
+            placedPos.forEach(pos -> assertFalse(playerField.isAvailable(pos)));
 
         }
     }
@@ -189,11 +191,11 @@ class PlayerFieldTest {
     @Test
     void getAvailablePositions() {
         Set<Position> availablePos = playerField.getAvailablePositions();
-        Assertions.assertTrue(availablePos.contains(Position.of(0, 0)));
-        Assertions.assertEquals(1, availablePos.size());
+        assertTrue(availablePos.contains(Position.of(0, 0)));
+        assertEquals(1, availablePos.size());
 
         // Place a StarterCard on its retro as they always have all corners available
-        Assertions.assertDoesNotThrow(() -> playerField.placeStartingCard(starterDeck.draw().orElseThrow(), true));
+        assertDoesNotThrow(() -> playerField.placeStartingCard(starterDeck.draw().orElseThrow(), true));
 
         Set<Position> availablePosAfterStarter = playerField.getAvailablePositions();
         Stream.of(Corner.values())
@@ -204,15 +206,15 @@ class PlayerFieldTest {
 
     @Test
     void isAvailable() {
-        Assertions.assertTrue(playerField.isAvailable(Position.of(0, 0)));
-        Assertions.assertFalse(playerField.isAvailable(Position.of(1, 1)));
+        assertTrue(playerField.isAvailable(Position.of(0, 0)));
+        assertFalse(playerField.isAvailable(Position.of(1, 1)));
 
         // Place a StarterCard on its retro as they always have all corners available
-        Assertions.assertDoesNotThrow(() -> playerField.placeStartingCard(starterDeck.draw().orElseThrow(), true));
+        assertDoesNotThrow(() -> playerField.placeStartingCard(starterDeck.draw().orElseThrow(), true));
         // Check that all corners are available
         Stream.of(Corner.values())
               .map(corner -> PositionManager.getPositionIn(Position.of(0, 0), corner))
-              .forEach(position -> Assertions.assertTrue(playerField.isAvailable(position)));
+              .forEach(position -> assertTrue(playerField.isAvailable(position)));
     }
 
     @Test
@@ -221,26 +223,26 @@ class PlayerFieldTest {
         Stream.concat(Stream.of(Color.values()),
                       Stream.of(Symbol.values()))
               .forEach(item -> {
-                  Assertions.assertEquals(0, playerField.getNumberOf(item));
+                  assertEquals(0, playerField.getNumberOf(item));
                   itemCount.put(item, 0);
               });
 
         // Place a StarterCard on its retro
         StarterCard starterCard = starterDeck.draw().orElseThrow();
-        Assertions.assertDoesNotThrow(() -> playerField.placeStartingCard(starterCard, true));
+        assertDoesNotThrow(() -> playerField.placeStartingCard(starterCard, true));
 
         // Checking that colors have been updated and symbol are 0 because of StarterCard retro
         Stream.of(Color.values())
               .forEach(color -> {
-                  Assertions.assertEquals(1, playerField.getNumberOf(color));
+                  assertEquals(1, playerField.getNumberOf(color));
                   itemCount.put(color, 1);
               });
         Stream.of(Symbol.values())
-              .forEach(symbol -> Assertions.assertEquals(0, playerField.getNumberOf(symbol)));
+              .forEach(symbol -> assertEquals(0, playerField.getNumberOf(symbol)));
 
         // Place a ResourceCard on (1, 1)
         ResourceCard resourceCard = resourceDeck.draw().orElseThrow();
-        Assertions.assertDoesNotThrow(() -> {
+        assertDoesNotThrow(() -> {
             playerField.place(resourceCard, Position.of(1, 1), false);
         });
 
@@ -255,55 +257,55 @@ class PlayerFieldTest {
               .forEach(item -> itemCount.merge(item, 1, Integer::sum));
 
         Stream.concat(Stream.of(Color.values()), Stream.of(Symbol.values()))
-              .forEach(item -> Assertions.assertEquals(itemCount.get(item), playerField.getNumberOf(item)));
+              .forEach(item -> assertEquals(itemCount.get(item), playerField.getNumberOf(item)));
 
     }
 
     @Test
     void clearAll() {
         StarterCard card = starterDeck.draw().orElseThrow();
-        Assertions.assertDoesNotThrow(() -> playerField.placeStartingCard(card, true));
+        assertDoesNotThrow(() -> playerField.placeStartingCard(card, true));
         playerField.clearAll();
-        Assertions.assertTrue(playerField.isAvailable(Position.of(0, 0)));
-        Assertions.assertFalse(playerField.containsCard(card));
+        assertTrue(playerField.isAvailable(Position.of(0, 0)));
+        assertFalse(playerField.containsCard(card));
 
         Stream.concat(Stream.of(Color.values()), Stream.of(Symbol.values()))
-              .forEach(item -> Assertions.assertEquals(0, playerField.getNumberOf(item)));
+              .forEach(item -> assertEquals(0, playerField.getNumberOf(item)));
         Stream.of(Color.values())
-              .forEach(color -> Assertions.assertEquals(0, playerField.getNumberOfPositionedColor(color)));
+              .forEach(color -> assertEquals(0, playerField.getNumberOfPositionedColor(color)));
     }
 
     @Test
     void containsCard() {
         StarterCard card = starterDeck.draw().orElseThrow();
-        Assertions.assertDoesNotThrow(() -> playerField.placeStartingCard(card, true));
-        Assertions.assertTrue(playerField.containsCard(card));
+        assertDoesNotThrow(() -> playerField.placeStartingCard(card, true));
+        assertTrue(playerField.containsCard(card));
 
         ResourceCard resourceCard = resourceDeck.draw().orElseThrow();
-        Assertions.assertDoesNotThrow(() -> playerField.place(resourceCard, Position.of(1, 1), true));
-        Assertions.assertTrue(playerField.containsCard(resourceCard));
-        Assertions.assertFalse(playerField.containsCard(resourceDeck.draw().orElseThrow()));
+        assertDoesNotThrow(() -> playerField.place(resourceCard, Position.of(1, 1), true));
+        assertTrue(playerField.containsCard(resourceCard));
+        assertFalse(playerField.containsCard(resourceDeck.draw().orElseThrow()));
     }
 
     @Test
     void getNumberOfPositionedColor() {
         StarterCard card = starterDeck.draw().orElseThrow();
-        Assertions.assertDoesNotThrow(() -> playerField.placeStartingCard(card, true));
+        assertDoesNotThrow(() -> playerField.placeStartingCard(card, true));
         Stream.of(Color.values())
-              .forEach(color -> Assertions.assertEquals(0, playerField.getNumberOfPositionedColor(color)));
+              .forEach(color -> assertEquals(0, playerField.getNumberOfPositionedColor(color)));
 
         ResourceCard resourceCard = resourceDeck.draw().orElseThrow();
-        Assertions.assertDoesNotThrow(() -> playerField.place(resourceCard,
-                                                              Position.of(1, 1),
-                                                              true));
-        Assertions.assertEquals(1, playerField.getNumberOfPositionedColor(resourceCard.getColor()));
+        assertDoesNotThrow(() -> playerField.place(resourceCard,
+                                                   Position.of(1, 1),
+                                                   true));
+        assertEquals(1, playerField.getNumberOfPositionedColor(resourceCard.getColor()));
     }
 
     @Test
     void isRequirementMet() {
         StarterCard card = starterDeck.draw().orElseThrow();
-        Assertions.assertTrue(playerField.isRequirementMet(card));
-        Assertions.assertTrue(playerField.isRequirementMet(resourceDeck.draw().orElseThrow()));
-        Assertions.assertFalse(playerField.isRequirementMet(goldDeck.draw().orElseThrow()));
+        assertTrue(playerField.isRequirementMet(card));
+        assertTrue(playerField.isRequirementMet(resourceDeck.draw().orElseThrow()));
+        assertFalse(playerField.isRequirementMet(goldDeck.draw().orElseThrow()));
     }
 }
--- src/test/java/it/polimi/ingsw/am11/players/field/PositionManagerTest.java
+++ src/test/java/it/polimi/ingsw/am11/players/field/PositionManagerTest.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.am11.players.field;
 
 import it.polimi.ingsw.am11.cards.utils.enums.Corner;
 import it.polimi.ingsw.am11.players.Position;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +9,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PositionManagerTest {
 
@@ -25,16 +27,16 @@ class PositionManagerTest {
     @Test
     void getPositionIn() {
         Position position = Position.of(0, 0);
-        Assertions.assertEquals(
+        assertEquals(
                 Position.of(1, 1),
                 PositionManager.getPositionIn(position, Corner.TOP_RX));
-        Assertions.assertEquals(
+        assertEquals(
                 Position.of(-1, 1),
                 PositionManager.getPositionIn(position, Corner.TOP_LX));
-        Assertions.assertEquals(
+        assertEquals(
                 Position.of(1, -1),
                 PositionManager.getPositionIn(position, Corner.DOWN_RX));
-        Assertions.assertEquals(
+        assertEquals(
                 Position.of(-1, -1),
                 PositionManager.getPositionIn(position, Corner.DOWN_LX));
 
@@ -43,16 +45,16 @@ class PositionManagerTest {
     @Test
     void getMovementOfPositions() {
         Position position = Position.of(0, 0);
-        Assertions.assertEquals(
+        assertEquals(
                 Position.of(1, 1),
                 PositionManager.getMovementOfPositions(position, Collections.singletonList(Corner.TOP_RX)));
-        Assertions.assertEquals(
+        assertEquals(
                 Position.of(-1, 1),
                 PositionManager.getMovementOfPositions(position, List.of(Corner.TOP_LX)));
-        Assertions.assertEquals(
+        assertEquals(
                 Position.of(0, 2),
                 PositionManager.getMovementOfPositions(position, List.of(Corner.TOP_RX, Corner.TOP_LX)));
-        Assertions.assertEquals(
+        assertEquals(
                 Position.of(2, -2),
                 PositionManager.getMovementOfPositions(position, List.of(Corner.DOWN_RX, Corner.DOWN_RX)));
     }
@@ -61,51 +63,51 @@ class PositionManagerTest {
     void getCornerFromPositions() {
         Position firstPos = Position.of(0, 0);
         Position secondPos = Position.of(-1, 1);
-        Assertions.assertEquals(Optional.of(Corner.TOP_LX),
-                                PositionManager.getCornerFromPositions(firstPos, secondPos));
+        assertEquals(Optional.of(Corner.TOP_LX),
+                     PositionManager.getCornerFromPositions(firstPos, secondPos));
 
         firstPos = Position.of(0, 0);
         secondPos = Position.of(1, 1);
-        Assertions.assertEquals(Optional.of(Corner.TOP_RX),
-                                PositionManager.getCornerFromPositions(firstPos, secondPos));
+        assertEquals(Optional.of(Corner.TOP_RX),
+                     PositionManager.getCornerFromPositions(firstPos, secondPos));
 
         firstPos = Position.of(0, 0);
         secondPos = Position.of(1, -1);
-        Assertions.assertEquals(Optional.of(Corner.DOWN_RX),
-                                PositionManager.getCornerFromPositions(firstPos, secondPos));
+        assertEquals(Optional.of(Corner.DOWN_RX),
+                     PositionManager.getCornerFromPositions(firstPos, secondPos));
 
         firstPos = Position.of(3, 3);
         secondPos = Position.of(2, 2);
-        Assertions.assertEquals(Optional.of(Corner.DOWN_LX),
-                                PositionManager.getCornerFromPositions(firstPos, secondPos));
+        assertEquals(Optional.of(Corner.DOWN_LX),
+                     PositionManager.getCornerFromPositions(firstPos, secondPos));
 
         firstPos = Position.of(0, 0);
         secondPos = Position.of(0, 0);
-        Assertions.assertEquals(Optional.empty(),
-                                PositionManager.getCornerFromPositions(firstPos, secondPos));
+        assertEquals(Optional.empty(),
+                     PositionManager.getCornerFromPositions(firstPos, secondPos));
 
         firstPos = Position.of(0, 0);
         secondPos = Position.of(0, 1);
-        Assertions.assertEquals(Optional.empty(),
-                                PositionManager.getCornerFromPositions(firstPos, secondPos));
+        assertEquals(Optional.empty(),
+                     PositionManager.getCornerFromPositions(firstPos, secondPos));
 
         firstPos = Position.of(0, 0);
         for (int i = 0; i < 100; i++) {
             secondPos = Position.of(gen.nextInt(2, 100), gen.nextInt(2, 100));
-            Assertions.assertEquals(Optional.empty(),
-                                    PositionManager.getCornerFromPositions(firstPos, secondPos));
+            assertEquals(Optional.empty(),
+                         PositionManager.getCornerFromPositions(firstPos, secondPos));
 
             secondPos = Position.of(gen.nextInt(-100, -2), gen.nextInt(-100, -2));
-            Assertions.assertEquals(Optional.empty(),
-                                    PositionManager.getCornerFromPositions(firstPos, secondPos));
+            assertEquals(Optional.empty(),
+                         PositionManager.getCornerFromPositions(firstPos, secondPos));
         }
     }
 
     @Test
     void reset() {
         positionManager.reset();
-        Assertions.assertEquals(1, positionManager.getAvailablePositions().size());
-        Assertions.assertEquals(0, positionManager.getCardsPositioned().size());
-        Assertions.assertTrue(positionManager.isAvailable(Position.of(0, 0)));
+        assertEquals(1, positionManager.getAvailablePositions().size());
+        assertEquals(0, positionManager.getCardsPositioned().size());
+        assertTrue(positionManager.isAvailable(Position.of(0, 0)));
     }
 }
