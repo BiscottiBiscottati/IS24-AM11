b208b60bf233c3b0b0f9bd00f5730056f76f760d
EdoardoBergamoMac <edoardo.bergamo@mail.polimi.it>
2024-04-16 00:23:15 +0200

    dded new tests to playermanager

--- src/test/java/it/polimi/ingsw/am11/model/PlayerManagerTest.java
+++ src/test/java/it/polimi/ingsw/am11/model/PlayerManagerTest.java
@@ -9,8 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerManagerTest {
 
@@ -37,6 +36,13 @@ class PlayerManagerTest {
     }
 
     @Test
+    void getMaxNumberOfPlayer() {
+        int val = 4;
+        PlayerManager.setMaxNumberOfPlayers(val);
+        assertEquals(val, PlayerManager.getMaxNumberOfPlayer());
+    }
+
+    @Test
     void getPlayers() {
         Set<String> modelPlayers;
         //Case with no players
@@ -57,6 +63,47 @@ class PlayerManagerTest {
 
         assertEquals(3, modelPlayers.size());
         assertEquals(players, modelPlayers);
+    }
+
+    @Test
+    void getNumberOfPlayers() {
+        assertEquals(0, manager.getNumberOfPlayers());
+
+        try {
+            manager.addPlayerToTable("player1", PlayerColor.BLUE);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(1, manager.getNumberOfPlayers());
+        try {
+            manager.addPlayerToTable("player2", PlayerColor.GREEN);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(2, manager.getNumberOfPlayers());
+        try {
+            manager.addPlayerToTable("player3", PlayerColor.RED);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(3, manager.getNumberOfPlayers());
+        try {
+            manager.addPlayerToTable("player4", PlayerColor.YELLOW);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(4, manager.getNumberOfPlayers());
+        manager.removePlayer("Giorgio");
+        assertEquals(4, manager.getNumberOfPlayers());
+        manager.removePlayer("Player1");
+        assertEquals(3, manager.getNumberOfPlayers());
+        manager.removePlayer("Player2");
+        assertEquals(2, manager.getNumberOfPlayers());
+        manager.removePlayer("Player3");
+        assertEquals(1, manager.getNumberOfPlayers());
+        manager.removePlayer("Player4");
+        assertEquals(0, manager.getNumberOfPlayers());
+
     }
 
     @Test
@@ -107,34 +154,186 @@ class PlayerManagerTest {
 
     @Test
     void getPlayer() {
+        Set<String> players = Set.of("player1", "player2", "player3");
+        try {
+            manager.addPlayerToTable("player1", PlayerColor.BLUE);
+            manager.addPlayerToTable("player2", PlayerColor.GREEN);
+            manager.addPlayerToTable("player3", PlayerColor.RED);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+        for (String nickname : players) {
+            assertEquals(nickname, manager.getPlayer(nickname).nickname());
+        }
 
+        assertNull(manager.getPlayer("Giorgio"));
     }
 
     @Test
     void getHand() {
+        Set<String> players = Set.of("player1", "player2", "player3", "player4");
+        try {
+            manager.addPlayerToTable("player1", PlayerColor.BLUE);
+            manager.addPlayerToTable("player2", PlayerColor.GREEN);
+            manager.addPlayerToTable("player3", PlayerColor.RED);
+            manager.addPlayerToTable("player4", PlayerColor.YELLOW);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+        //TODO
     }
 
     @Test
     void getPlayerObjective() {
+        //TODO
     }
 
     @Test
     void getPlayerColor() {
+        Set<String> players = Set.of("player1", "player2", "player3", "player4");
+        try {
+            manager.addPlayerToTable("player1", PlayerColor.BLUE);
+            manager.addPlayerToTable("player2", PlayerColor.GREEN);
+            manager.addPlayerToTable("player3", PlayerColor.RED);
+            manager.addPlayerToTable("player4", PlayerColor.YELLOW);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(manager.getPlayerColor("Player1"), PlayerColor.BLUE);
+        assertEquals(manager.getPlayerColor("Player2"), PlayerColor.GREEN);
+        assertEquals(manager.getPlayerColor("Player3"), PlayerColor.RED);
+        assertEquals(manager.getPlayerColor("Player4"), PlayerColor.YELLOW);
     }
 
     @Test
     void addPlayerToTable() {
+        //reducing the limit to test exceptions
+        PlayerManager.setMaxNumberOfPlayers(3);
+        //adding 1 player
+        try {
+            manager.addPlayerToTable("player1", PlayerColor.BLUE);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+
+        //adding a player with same name or color
+        assertThrows(PlayerInitException.class,
+                     () -> manager.addPlayerToTable("player1", PlayerColor.YELLOW));
+        assertThrows(PlayerInitException.class,
+                     () -> manager.addPlayerToTable("player2", PlayerColor.BLUE));
+
+        //reaching the limit
+        try {
+            manager.addPlayerToTable("player2", PlayerColor.GREEN);
+            manager.addPlayerToTable("player3", PlayerColor.RED);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+
+        //overcoming the limit
+        assertThrows(PlayerInitException.class,
+                     () -> manager.addPlayerToTable("player4", PlayerColor.YELLOW));
+
+
     }
 
     @Test
     void removePlayer() {
+        try {
+            manager.addPlayerToTable("player1", PlayerColor.BLUE);
+            manager.addPlayerToTable("player2", PlayerColor.GREEN);
+            manager.addPlayerToTable("player3", PlayerColor.RED);
+            manager.addPlayerToTable("player4", PlayerColor.YELLOW);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+
+        //removing a player
+        manager.removePlayer("Player1");
+        assertNull(manager.getPlayer("Player1"));
+        Set<String> players = Set.of("player2", "player3", "player4");
+        assertEquals(players, manager.getPlayers());
+
+        //removing a fake player
+        manager.removePlayer("Giorgio");
+        assertEquals(players, manager.getPlayers());
     }
 
     @Test
     void isFirstTheCurrent() {
+        Set<String> players = Set.of("player1", "player2", "player3", "player4");
+        try {
+            manager.addPlayerToTable("player1", PlayerColor.BLUE);
+            manager.addPlayerToTable("player2", PlayerColor.GREEN);
+            manager.addPlayerToTable("player3", PlayerColor.RED);
+            manager.addPlayerToTable("player4", PlayerColor.YELLOW);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+
+        manager.startingTheGame();
+
+        assertTrue(manager.isFirstTheCurrent());
+
+        for (int i = 0; i < players.size(); i++) {
+            goNextTurn();
+        }
+        assertTrue(manager.isFirstTheCurrent());
     }
 
     @Test
     void goNextTurn() {
+        Set<String> players = Set.of("player1", "player2", "player3", "player4");
+        try {
+            manager.addPlayerToTable("player1", PlayerColor.BLUE);
+            manager.addPlayerToTable("player2", PlayerColor.GREEN);
+            manager.addPlayerToTable("player3", PlayerColor.RED);
+            manager.addPlayerToTable("player4", PlayerColor.YELLOW);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+
+        int numOfPlayers = players.size();
+        ArrayList<String> orderOfPlayers = new ArrayList<>(numOfPlayers);
+        assertEquals(manager.getFirstPlayer(), manager.getCurrentTurnPlayer());
+
+        for (int i = 0; i < numOfPlayers; i++) {
+            orderOfPlayers.add(i, manager.getCurrentTurnPlayer());
+            manager.goNextTurn();
+        }
+        for (int j = 0; j < numOfPlayers; j++) {
+            assertEquals(orderOfPlayers.get(j), manager.getCurrentTurnPlayer());
+            manager.goNextTurn();
+        }
+
+
+    }
+
+    @Test
+    void startingTheGame() {
+        Set<String> players = Set.of("player1", "player2", "player3", "player4");
+        try {
+            manager.addPlayerToTable("player1", PlayerColor.BLUE);
+            manager.addPlayerToTable("player2", PlayerColor.GREEN);
+            manager.addPlayerToTable("player3", PlayerColor.RED);
+            manager.addPlayerToTable("player4", PlayerColor.YELLOW);
+        } catch (PlayerInitException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertNull(manager.getFirstPlayer());
+        assertNull(manager.getCurrentTurnPlayer());
+
+        manager.startingTheGame();
+
+        assertNotNull(manager.getFirstPlayer());
+        assertNotNull(manager.getCurrentTurnPlayer());
+        assertTrue(manager.isFirstTheCurrent());
+    }
+
+    @Test
+    void resetAll() {
+        //TODO
     }
 }
