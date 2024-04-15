662c472502aa5c2f2eb5904de7f869e446c3a832
EdoardoBergamoMac <edoardo.bergamo@mail.polimi.it>
2024-04-15 15:47:41 +0200

    addd test file

---
+++ src/test/java/it/polimi/ingsw/am11/model/GameLogicTest.java
@@ -0,0 +1,143 @@
+package it.polimi.ingsw.am11.model;
+
+import org.junit.jupiter.api.Test;
+
+class GameLogicTest {
+    
+    @Test
+    void getPlayers() {
+
+    }
+
+    @Test
+    void getCurrentTurnPlayer() {
+    }
+
+    @Test
+    void getFirstPlayer() {
+    }
+
+    @Test
+    void getPlayerHand() {
+    }
+
+    @Test
+    void getPlayerObjective() {
+    }
+
+    @Test
+    void getPlayerColor() {
+    }
+
+    @Test
+    void getPositionedCard() {
+    }
+
+    @Test
+    void getAvailablePositions() {
+    }
+
+    @Test
+    void getResourceDeckTop() {
+    }
+
+    @Test
+    void getGoldDeckTop() {
+    }
+
+    @Test
+    void getCommonObjectives() {
+    }
+
+    @Test
+    void getExposedGoldsCrd() {
+    }
+
+    @Test
+    void getExposedResourcesCrd() {
+    }
+
+    @Test
+    void getPlayerPoints() {
+    }
+
+    @Test
+    void isArmageddonTime() {
+    }
+
+    @Test
+    void getPlayerFinishingPosition() {
+    }
+
+    @Test
+    void getWinner() {
+    }
+
+    @Test
+    void initGame() {
+    }
+
+    @Test
+    void addPlayerToTable() {
+    }
+
+    @Test
+    void removePlayer() {
+    }
+
+    @Test
+    void shufflePlayers() {
+    }
+
+    @Test
+    void setStartingPlayer() {
+    }
+
+    @Test
+    void pickStarter() {
+    }
+
+    @Test
+    void pickObjective() {
+    }
+
+    @Test
+    void setStarterFor() {
+    }
+
+    @Test
+    void setObjectiveFor() {
+    }
+
+    @Test
+    void goNextTurn() {
+    }
+
+    @Test
+    void placeCard() {
+    }
+
+    @Test
+    void drawFromGoldDeck() {
+    }
+
+    @Test
+    void drawFromResourceDeck() {
+    }
+
+    @Test
+    void drawVisibleGold() {
+    }
+
+    @Test
+    void drawVisibleResource() {
+    }
+
+    @Test
+    void countObjectivesPoints() {
+    }
+
+    @Test
+    void endGame() {
+    }
+}
