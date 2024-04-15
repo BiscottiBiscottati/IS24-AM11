8c35942837a0e935171e5f57f3215576690f5ee3
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-04-15 15:16:44 +0200

    removed compare method

--- src/main/java/it/polimi/ingsw/am11/players/Position.java
+++ src/main/java/it/polimi/ingsw/am11/players/Position.java
@@ -3,17 +3,10 @@ package it.polimi.ingsw.am11.players;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-public record Position(int x, int y) implements Comparable<Position> {
+public record Position(int x, int y) {
     @Contract("_, _ -> new")
     public static @NotNull Position of(int x, int y) {
         return new Position(x, y);
     }
 
-    @Override
-    public int compareTo(@NotNull Position o) {
-        if (this.x == o.x) {
-            return Integer.compare(this.y, o.y);
-        }
-        return Integer.compare(this.x, o.x);
-    }
 }
