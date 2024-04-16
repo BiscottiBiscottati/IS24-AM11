34dd8ef94f4ce8712f26e3535e1de3298ff794d2
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-04-16 15:27:06 +0200

    suppress for some warning

--- src/main/java/it/polimi/ingsw/am11/model/GameModel.java
+++ src/main/java/it/polimi/ingsw/am11/model/GameModel.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
+@SuppressWarnings({"ClassWithTooManyMethods", "OverlyCoupledClass"})
 public interface GameModel {
 
     Set<String> getPlayers();
