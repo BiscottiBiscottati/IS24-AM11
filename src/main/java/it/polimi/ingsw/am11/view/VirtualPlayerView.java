d728034f1eb4555592ceca29cc22f400d7a7a0ea
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-04-27 16:25:32 +0200

    added some empty methods to player view

--- src/main/java/it/polimi/ingsw/am11/view/VirtualPlayerView.java
+++ src/main/java/it/polimi/ingsw/am11/view/VirtualPlayerView.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.am11.view;
 import it.polimi.ingsw.am11.controller.CardController;
 import it.polimi.ingsw.am11.controller.CentralController;
 import it.polimi.ingsw.am11.controller.GameController;
+import it.polimi.ingsw.am11.model.cards.utils.enums.PlayableCardType;
 import it.polimi.ingsw.am11.network.PlayerConnector;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,5 +22,17 @@ public class VirtualPlayerView {
     public VirtualPlayerView(@NotNull PlayerConnector connector, @NotNull String nickname) {
         this.connector = connector;
         this.nickname = nickname;
+    }
+
+    public void setStarterCard(boolean isRetro) {
+    }
+
+    public void setObjectiveCard(int cardId) {
+    }
+
+    public void placeCard(int cardId, int x, int y, boolean isRetro) {
+    }
+
+    public void drawCard(boolean fromVisible, PlayableCardType type, int cardId) {
     }
 }
