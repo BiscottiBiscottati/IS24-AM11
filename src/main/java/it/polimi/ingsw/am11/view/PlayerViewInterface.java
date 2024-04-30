d5fdc65ed7d1a73a5d5af6c9b989d997e2753157
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-04-30 13:37:03 +0200

    added exceptions throw in VirtualPlayerView

--- src/main/java/it/polimi/ingsw/am11/view/PlayerViewInterface.java
+++ src/main/java/it/polimi/ingsw/am11/view/PlayerViewInterface.java
@@ -1,16 +1,23 @@
 package it.polimi.ingsw.am11.view;
 
 import it.polimi.ingsw.am11.model.cards.utils.enums.PlayableCardType;
+import it.polimi.ingsw.am11.model.exceptions.*;
 
 import java.rmi.Remote;
 
 public interface PlayerViewInterface extends Remote {
 
-    void setStarterCard(boolean isRetro);
+    void setStarterCard(boolean isRetro)
+    throws PlayerInitException, IllegalCardPlacingException, GameStatusException;
 
-    void setObjectiveCard(int cardId);
+    void setObjectiveCard(int cardId)
+    throws IllegalPlayerSpaceActionException, PlayerInitException, GameStatusException;
 
-    void placeCard(int cardId, int x, int y, boolean isRetro);
+    void placeCard(int cardId, int x, int y, boolean isRetro)
+    throws TurnsOrderException, PlayerInitException, IllegalCardPlacingException,
+           NotInHandException, IllegalPlateauActionException, GameStatusException;
 
-    void drawCard(boolean fromVisible, PlayableCardType type, int cardId);
+    void drawCard(boolean fromVisible, PlayableCardType type, int cardId)
+    throws IllegalPlayerSpaceActionException, TurnsOrderException, IllegalPickActionException,
+           PlayerInitException, EmptyDeckException, MaxHandSizeException, GameStatusException;
 }
