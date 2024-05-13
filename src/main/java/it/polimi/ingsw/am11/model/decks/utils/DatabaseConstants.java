3f3a8aa76bff95ce71422d647bc8dcff8648b04a
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-05-13 23:37:02 +0200

    fixed way to getting database when packaged to jar

--- src/main/java/it/polimi/ingsw/am11/model/decks/utils/DatabaseConstants.java
+++ src/main/java/it/polimi/ingsw/am11/model/decks/utils/DatabaseConstants.java
@@ -1,5 +1,10 @@
 package it.polimi.ingsw.am11.model.decks.utils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * This class contains constants that are used for database operations.
  */
@@ -10,7 +15,29 @@ public class DatabaseConstants {
      * Uses the <code>getResource</code> method to locate the path of "/db/cards.sqlite" in the
      * resources' folder.
      */
-    public static final String DATABASE_URL =
-            "jdbc:sqlite:" +
-            DatabaseConstants.class.getResource("/db/cards.sqlite");
+    public static final String DATABASE_URL;
+
+    static {
+        File tempDbFile;
+        try (InputStream inputStream = DatabaseConstants.class.getResourceAsStream(
+                "/db/cards.sqlite")) {
+
+            // check if the database file is found
+            if (inputStream == null) {
+                throw new RuntimeException("Database file not found");
+            }
+
+            // create a temporary file to store the database
+            tempDbFile = File.createTempFile("cards", ".sqlite");
+
+            // copy the database data to the temporary file
+            try (FileOutputStream outputStream = new FileOutputStream(tempDbFile)) {
+                inputStream.transferTo(outputStream);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        DATABASE_URL = "jdbc:sqlite:" + tempDbFile.getAbsolutePath();
+    }
 }
