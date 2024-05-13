db987b15d4fccd04c0820a0a41ce5a6077adc0aa
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-05-14 00:49:20 +0200

    added temporary gui start

--- src/main/java/it/polimi/ingsw/am11/Client.java
+++ src/main/java/it/polimi/ingsw/am11/Client.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.am11;
 
 import it.polimi.ingsw.am11.utils.ArgParser;
+import it.polimi.ingsw.am11.view.client.GUI.window.CodexNaturalis;
+import javafx.application.Application;
 import org.jetbrains.annotations.NotNull;
 
 public class Client {
@@ -8,5 +10,11 @@ public class Client {
     static void start(@NotNull ArgParser parser) {
         String uiMode = parser.getOption("ui").orElseThrow().getValue();
         // TODO gui or tui and setup connection to server
+        switch (uiMode) {
+            case "gui" -> Application.launch(CodexNaturalis.class);
+            case "tui" -> System.out.println("TUI not implemented yet");
+            default -> System.out.println("Invalid UI mode " + uiMode + ". Please choose between " +
+                                          "'gui' and 'tui'");
+        }
     }
 }
