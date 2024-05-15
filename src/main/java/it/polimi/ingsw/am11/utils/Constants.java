b8fbc870be061240207f02278cccf79fc4827068
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-05-15 18:08:08 +0200

    changed constants

--- src/main/java/it/polimi/ingsw/am11/utils/Constants.java
+++ src/main/java/it/polimi/ingsw/am11/utils/Constants.java
@@ -2,5 +2,5 @@ package it.polimi.ingsw.am11.utils;
 
 public class Constants {
     public static final int DEFAULT_SOCKET_PORT = 12345;
-    public static final int DEFAULT_RMI_PORT = 12346;
+    public static final int DEFAULT_RMI_PORT = 54321;
 }
