cb160222989f03b36b3c71fb2aa7589de22ca346
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-05-20 23:46:47 +0200

    field cover logic (not tested)

--- src/main/java/it/polimi/ingsw/am11/view/client/miniModel/CliField.java
+++ src/main/java/it/polimi/ingsw/am11/view/client/miniModel/CliField.java
@@ -1,20 +1,30 @@
 package it.polimi.ingsw.am11.view.client.miniModel;
 
+import it.polimi.ingsw.am11.model.cards.utils.enums.Corner;
+import it.polimi.ingsw.am11.model.players.field.PositionManager;
 import it.polimi.ingsw.am11.model.players.utils.Position;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class CliField {
     private final @NotNull Map<Position, MiniCardContainer> cardsPositioned;
 
     public CliField() {
-        cardsPositioned = new HashMap<>();
+        cardsPositioned = new HashMap<>(20);
     }
 
     public void place(Position pos, int cardId, boolean isRetro) {
-        cardsPositioned.put(pos, new MiniCardContainer(cardId, isRetro));
+
+        MiniCardContainer cardContainer = new MiniCardContainer(cardId, isRetro);
+        Stream.of(Corner.values())
+              .filter(corner -> cardsPositioned.containsKey(PositionManager.getPositionIn(pos,
+                                                                                          corner)))
+              .forEach(cardContainer::cover);
+
+        cardsPositioned.put(pos, cardContainer);
     }
 
     public void remove(Position pos) {
