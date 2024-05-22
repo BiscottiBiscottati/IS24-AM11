6e6fee04beee75d8cdd5a597ebc4da7bf4bd570f
biscotti-uni-linux <zining.chen@mail.polimi.it>
2024-05-22 23:51:54 +0200

    test print for cards

--- src/test/java/it/polimi/ingsw/am11/view/client/TUI/printers/CardPrinterTest.java
+++ src/test/java/it/polimi/ingsw/am11/view/client/TUI/printers/CardPrinterTest.java
@@ -1,20 +1,64 @@
 package it.polimi.ingsw.am11.view.client.TUI.printers;
 
 import it.polimi.ingsw.am11.model.cards.objective.ObjectiveCard;
+import it.polimi.ingsw.am11.model.cards.playable.GoldCard;
+import it.polimi.ingsw.am11.model.cards.playable.ResourceCard;
+import it.polimi.ingsw.am11.model.cards.starter.StarterCard;
 import it.polimi.ingsw.am11.model.decks.Deck;
 import it.polimi.ingsw.am11.model.decks.objective.ObjectiveDeckFactory;
+import it.polimi.ingsw.am11.model.decks.playable.GoldDeckFactory;
+import it.polimi.ingsw.am11.model.decks.playable.ResourceDeckFactory;
+import it.polimi.ingsw.am11.model.decks.starter.StarterDeckFactory;
 import it.polimi.ingsw.am11.model.exceptions.IllegalCardBuildException;
 import org.junit.jupiter.api.Test;
 
 class CardPrinterTest {
 
     @Test
-    void printCardFrontAndBack() throws IllegalCardBuildException {
+    void printObjectiveCards() throws IllegalCardBuildException {
         Deck<ObjectiveCard> objDeck = ObjectiveDeckFactory.createDeck();
 
         int remainingCards = objDeck.getRemainingCards();
         for (int i = 0; i < remainingCards; i++) {
-            CardPrinter.printCardFrontAndBack(objDeck.draw().orElseThrow().getId());
+            int id = objDeck.draw().orElseThrow().getId();
+            System.out.println("Card id: " + id);
+            CardPrinter.printCardFrontAndBack(id);
+        }
+    }
+
+    @Test
+    void printStartCard() throws IllegalCardBuildException {
+        Deck<StarterCard> starterDeck = StarterDeckFactory.createDeck();
+
+        int remainingCards = starterDeck.getRemainingCards();
+        for (int i = 0; i < remainingCards; i++) {
+            int id = starterDeck.draw().orElseThrow().getId();
+            System.out.println("Card id : " + id);
+            CardPrinter.printCardFrontAndBack(id);
+        }
+    }
+
+    @Test
+    void printResourceCards() throws IllegalCardBuildException {
+        Deck<ResourceCard> resourceDeck = ResourceDeckFactory.createDeck();
+
+        int remainingCards = resourceDeck.getRemainingCards();
+        for (int i = 0; i < remainingCards; i++) {
+            int id = resourceDeck.draw().orElseThrow().getId();
+            System.out.println("Card id : " + id);
+            CardPrinter.printCardFrontAndBack(id);
+        }
+    }
+
+    @Test
+    void printGoldCard() throws IllegalCardBuildException {
+        Deck<GoldCard> goldDeck = GoldDeckFactory.createDeck();
+
+        int remainingCards = goldDeck.getRemainingCards();
+        for (int i = 0; i < remainingCards; i++) {
+            int id = goldDeck.draw().orElseThrow().getId();
+            System.out.println("Card id : " + id);
+            CardPrinter.printCardFrontAndBack(id);
         }
     }
 }
