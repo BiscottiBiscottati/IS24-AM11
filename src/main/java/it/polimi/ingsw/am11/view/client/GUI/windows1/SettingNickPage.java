9cda385d5224f0d6dd88d9bf4932117e6e5816c7
Osama <osama.atiqi@mail.polimi.it>
2024-05-30 17:06:46 +0200

    implementing GUI

--- src/main/java/it/polimi/ingsw/am11/view/client/GUI/windows1/SettingNickPage.java
+++ src/main/java/it/polimi/ingsw/am11/view/client/GUI/windows1/SettingNickPage.java
@@ -20,7 +20,8 @@ public class SettingNickPage {
     Label yourName, nameAlreadyTaken;
     Font font, fontBig;
     Button chooseNick;
-    int halfButtonSize;
+    Button goToNetwork;
+    int halfButtonSize, distanceToBorder;
 
 
     public SettingNickPage(CodexNaturalis1 codexNaturalis) {
@@ -33,20 +34,17 @@ public class SettingNickPage {
         font = codexNaturalis.getFont();
         fontBig = codexNaturalis.getFontBig();
         halfButtonSize = codexNaturalis.getHalfButtonSize();
+        distanceToBorder = codexNaturalis.getDistanceToBorder();
         writeNick = new TextField();
-        yourName = new Label("Your Name:");
-        nameAlreadyTaken = new Label();
-        root.getChildren().addAll(yourName, writeNick, nameAlreadyTaken);
-        chooseNick = new Button("_Cancel");
-        root.getChildren().addAll(chooseNick);
-    }
 
-    public void showSettingNickPage() {
         writeNick.setAlignment(Pos.CENTER);
         writeNick.setPromptText("Nickname");
         writeNick.setFont(font);
         writeNick.setStyle("-fx-background-color: #D7BC49; -fx-background-radius: 5;" +
                            " -fx-max-width: " + 20 * halfButtonSize);
+
+        yourName = new Label("Your Name:");
+        nameAlreadyTaken = new Label();
 
         yourName.setBackground(Background.EMPTY);
         yourName.setFont(fontBig);
@@ -57,10 +55,53 @@ public class SettingNickPage {
         nameAlreadyTaken.setText("Name already taken");
         nameAlreadyTaken.setTextFill(Color.RED);
 
+        root.getChildren().addAll(yourName, writeNick, nameAlreadyTaken);
+        chooseNick = new Button("_Cancel");
+        goToNetwork = new Button("_Cancel");
+
+        goToNetwork.setPrefSize(halfButtonSize << 3, halfButtonSize << 1);
+        goToNetwork.setTranslateX(- 5 * halfButtonSize);
+        goToNetwork.setTranslateY(10 * halfButtonSize - distanceToBorder);
+        goToNetwork.setStyle("-fx-background-color: #D7BC49; -fx-background-radius: 5");
+        goToNetwork.setText("go Back");
+        goToNetwork.setFont(font);
+        goToNetwork.setTextFill(Color.web("#351F17"));
+        goToNetwork.setOnMousePressed(
+                event -> goToNetwork.setStyle(
+                        "-fx-background-color: #685C19; -fx-background-radius: 5"));
+        goToNetwork.setOnMouseReleased(event -> goToNetwork.setStyle(
+                "-fx-background-color: #D7BC49; -fx-background-radius: 5"));
+
+        chooseNick.setPrefSize(halfButtonSize << 3, halfButtonSize << 1);
+        chooseNick.setTranslateX(5 * halfButtonSize);
+        chooseNick.setTranslateY(10 * halfButtonSize - distanceToBorder);
+        chooseNick.setStyle("-fx-background-color: #D7BC49; -fx-background-radius: 5");
+        chooseNick.setText("Choose");
+        chooseNick.setFont(font);
+        chooseNick.setTextFill(Color.web("#351F17"));
+        chooseNick.setOnMousePressed(
+                event -> chooseNick.setStyle(
+                        "-fx-background-color: #685C19; -fx-background-radius: 5"));
+        chooseNick.setOnMouseReleased(event -> chooseNick.setStyle(
+                "-fx-background-color: #D7BC49; -fx-background-radius: 5"));
+
+        root.getChildren().addAll(chooseNick, goToNetwork);
+
+        writeNick.setVisible(false);
+        yourName.setVisible(false);
+        nameAlreadyTaken.setVisible(false);
+        chooseNick.setVisible(false);
+        goToNetwork.setVisible(false);
+    }
+
+    public void showSettingNickPage() {
+
         yourName.setVisible(true);
         writeNick.setVisible(true);
         nameAlreadyTaken.setVisible(false);
         chooseNick.setVisible(true);
+        goToNetwork.setVisible(true);
+
         chooseNick.setOnMouseClicked(event -> {
             String nick = writeNick.getCharacters().toString();
             if (nick.isEmpty()) {
@@ -75,8 +116,18 @@ public class SettingNickPage {
                 yourName.setVisible(false);
                 nameAlreadyTaken.setVisible(false);
 
-                //goToWaitingRommPage();
+                //goToWaitingRoomPage();
             }
+        });
+
+        goToNetwork.setOnMouseClicked(event -> {
+            chooseNick.setVisible(false);
+            writeNick.setVisible(false);
+            yourName.setVisible(false);
+            nameAlreadyTaken.setVisible(false);
+            goToNetwork.setVisible(false);
+
+            codexNaturalis.showNetworkPage();
         });
     }
 }
--- src/main/java/it/polimi/ingsw/am11/view/client/GUI/windows1/WaitingRoomPage.java
+++ src/main/java/it/polimi/ingsw/am11/view/client/GUI/windows1/WaitingRoomPage.java
@@ -46,5 +46,6 @@ public class WaitingRoomPage {
 
     public void showWaitingRoomPage() {
 
+
     }
 }
